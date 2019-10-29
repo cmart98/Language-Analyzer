0ce40ce848561cd755553e8e2a6339903c7ce497
Christian Martinez <55301339+cmart98@users.noreply.github.com>
2019-10-29 14:21:54 -0500

    Update ask2.py

--- ask2.py
+++ ask2.py
@@ -10,21 +10,15 @@ def language_analysis(text):
     from google.cloud.language import enums
     from google.cloud.language import types
 
-    client = language.LanguageServiceClient()   ###  Hashed code is Old code as per video 
-                   #document = client.document_from_text(text)
+    client = language.LanguageServiceClient()   
+               
     document = types.Document(content=text, type=enums.Document.Type.PLAIN_TEXT)
-                                   #sent_analysis = client.analyze_sentiment()
+                    
     sent_analysis = client.analyze_sentiment(document=document).document_sentiment
-                                                            #sentiment = sent_analysis.sentiment
-                                                                    #ent_analysis = document.analyze_entities()
+                                   
     entities = client.analyze_entities(document).entities
-                                                                               #entities = ent_analysis.entities
     return sent_analysis, entities
-                                                                                        #example = u'is it not obvious that python is the best programming language'
-                                                                                        ### content taken from Python wiki pagei
-# example = "I want perfume"
-# example = raw_input("Search: ")
-
+                                                                                    
 # Instantiates a client
 client = speech.SpeechClient()
 
